36e100fd58cc63c4d04f9680113c153c62ad1499
Udit Narayan Ray <91251041+Udit-Narayan-Ray@users.noreply.github.com>
2022-11-29 21:14:36 +0530

    Update signinback.php

--- signinoutproject/signinback.php
+++ signinoutproject/signinback.php
@@ -6,21 +6,20 @@ include 'con.php';
 
 if(isset($_POST['signin']))
 {
-    session_start(); 
+    session_start(); //this will start the session of the login 
     $_SESSION['msg']='Login Please';
     $email=$_POST['email'];
     $pass=$_POST['password'];
 
-       //this will start the session of the login 
+       
     $query=mysqli_query($con,"select * from register where email='$email' and status='active'");
     $emailc=mysqli_num_rows($query);
     if($emailc)
     {
                 $validemail=mysqli_fetch_assoc($query);
-                $_SESSION['email']=$validemail['email'];     //this will store the session value for the login credential
-        //which will match with the visiting page if not destroy then will remain in same page otherwise if destroy then directed to signin form
+                $_SESSION['email']=$validemail['email'];     
                 $passv=$validemail['password'];
-                $pd=password_verify($pass,$passv);//password_verify(<input password>,<encrpted password>);
+                $pd=password_verify($pass,$passv);
                 if(!$pd)
                 {
                     
