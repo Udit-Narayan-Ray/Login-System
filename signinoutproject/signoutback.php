e1417f6ba448575fcb675755f9785b9b3db33de7
Udit Narayan Ray <91251041+Udit-Narayan-Ray@users.noreply.github.com>
2022-11-29 21:19:14 +0530

    update signoutback.php

--- signinoutproject/signoutback.php
+++ signinoutproject/signoutback.php
@@ -8,7 +8,7 @@ if(isset($_POST['signout']))
     session_start();
     echo '<script>alert("Log Out Successfully")</script>';
     session_destroy();
-    // echo header('location:signin.php');
+  
     echo '<script>location.replace("signin.php")</script>';
 }
 
